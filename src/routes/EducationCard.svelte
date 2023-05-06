9b7e5640c028c854c1f74199912207d42b019adf
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-05-06 17:50:29 +0200

    Added Education Section

---
+++ src/routes/EducationCard.svelte
@@ -0,0 +1,97 @@
+<script>
+	export let icon;
+	export let altText;
+	export let duration;
+	export let grade;
+	export let type;
+	export let description;
+</script>
+
+<div class="card">
+	<div class="icon">
+		<img src={icon} alt={altText} />
+	</div>
+	<div class="details">
+		<div class="tags">
+			<p class="tag">{duration}</p>
+			{#if grade !== undefined}<p class="tag">{grade}</p>{/if}
+		</div>
+		<div class="text">
+			<p class="title">
+				{type}
+			</p>
+			<p class="description">
+				{description}
+			</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.card {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+
+		background-color: var(--text-color);
+		padding: 1rem 1rem;
+		width: 100%;
+
+		border-radius: 10px;
+
+		transition: scale 0.5s var(--ease-out-expo);
+	}
+
+	.card:hover {
+		scale: 1.05;
+	}
+
+	.card * {
+		color: var(--background-color);
+	}
+
+	.icon {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		height: 100%;
+		max-height: 5rem;
+		padding: 0.5rem;
+		background-color: var(--primary-color);
+
+		border-radius: 50%;
+		aspect-ratio: 1/1;
+	}
+
+	.details {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		gap: 0.5rem;
+	}
+
+	.tags {
+		display: flex;
+		gap: 0.5rem;
+	}
+
+	.tag {
+		background-color: var(--background-color);
+		width: fit-content;
+		padding: 0.25rem 1rem;
+
+		color: var(--primary-color);
+		text-align: center;
+		font-size: 12px;
+
+		border-radius: 10px;
+	}
+
+	.text {
+	}
+
+	.title {
+		font-weight: bold;
+	}
+</style>
--- src/routes/EducationSection.svelte
+++ src/routes/EducationSection.svelte
@@ -1,10 +1,68 @@
-<h1 class="caption">Education</h1>
+<script>
+	import EducationCard from './EducationCard.svelte';
+</script>
+
+<div class="cards">
+	<EducationCard
+		icon="/icons/university.svg"
+		altText="university icon"
+		duration="2014-2021"
+		grade="1,8"
+		type="A-Level"
+		description="Friedrich Anton von Heinitz Gymnasium"
+	/>
+	<EducationCard
+		icon="/icons/university.svg"
+		altText="university icon"
+		duration="2021-2024"
+		grade="?"
+		type="Dual Study Computer Science (Theory Semesters)"
+		description="Highschool for Economics and Law Berlin"
+	/>
+	<EducationCard
+		icon="/icons/university.svg"
+		altText="university icon"
+		duration="2021-2024"
+		type="Dual Study Computer Science (Practical Semesters)"
+		description="Bosch Siemens Householding GmbH"
+	/>
+</div>
+<div class="education-image">
+	<img src="/education.svg" alt="women reading while standing on a book" />
+</div>
 
 <style>
-	.caption {
-		grid-column-start: 6;
-		grid-column-end: 8;
+	.cards {
+		grid-column: span 6;
+		grid-row: span 3;
 
-		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 2rem;
+	}
+
+	.education-image {
+		grid-column: span 6;
+		grid-row: span 3;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.education-image img {
+		height: 80%;
+		object-fit: contain;
+	}
+
+	@media (max-width: 1000px) {
+		.education-image {
+			display: none;
+		}
+
+		.cards {
+			grid-column: span 12;
+		}
 	}
 </style>
